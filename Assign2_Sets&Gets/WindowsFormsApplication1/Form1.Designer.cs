6e8096c506a29bfb985acf207e72a7c1dc242a6d
Nicholas <naricci@email.neit.edu>
2017-04-05 10:32:56 -0400

    add sets/gets and move Person class to its own file

---
+++ Assign2_Sets&Gets/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.txtMName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtStreet1 = new System.Windows.Forms.TextBox();
+            this.txtStreet2 = new System.Windows.Forms.TextBox();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.lblMName = new System.Windows.Forms.Label();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.lblStreet1 = new System.Windows.Forms.Label();
+            this.lblStreet2 = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblZipCode = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblFeedback = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(78, 42);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(128, 20);
+            this.txtFName.TabIndex = 0;
+            // 
+            // txtMName
+            // 
+            this.txtMName.Location = new System.Drawing.Point(307, 42);
+            this.txtMName.Name = "txtMName";
+            this.txtMName.Size = new System.Drawing.Size(115, 20);
+            this.txtMName.TabIndex = 1;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(511, 42);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(154, 20);
+            this.txtLName.TabIndex = 2;
+            // 
+            // txtStreet1
+            // 
+            this.txtStreet1.Location = new System.Drawing.Point(78, 77);
+            this.txtStreet1.Name = "txtStreet1";
+            this.txtStreet1.Size = new System.Drawing.Size(223, 20);
+            this.txtStreet1.TabIndex = 3;
+            // 
+            // txtStreet2
+            // 
+            this.txtStreet2.Location = new System.Drawing.Point(442, 77);
+            this.txtStreet2.Name = "txtStreet2";
+            this.txtStreet2.Size = new System.Drawing.Size(223, 20);
+            this.txtStreet2.TabIndex = 4;
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Location = new System.Drawing.Point(12, 45);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(60, 13);
+            this.lblFName.TabIndex = 5;
+            this.lblFName.Text = "First Name:";
+            // 
+            // lblMName
+            // 
+            this.lblMName.AutoSize = true;
+            this.lblMName.Location = new System.Drawing.Point(229, 45);
+            this.lblMName.Name = "lblMName";
+            this.lblMName.Size = new System.Drawing.Size(72, 13);
+            this.lblMName.TabIndex = 6;
+            this.lblMName.Text = "Middle Name:";
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Location = new System.Drawing.Point(444, 45);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(61, 13);
+            this.lblLName.TabIndex = 7;
+            this.lblLName.Text = "Last Name:";
+            // 
+            // lblStreet1
+            // 
+            this.lblStreet1.AutoSize = true;
+            this.lblStreet1.Location = new System.Drawing.Point(12, 80);
+            this.lblStreet1.Name = "lblStreet1";
+            this.lblStreet1.Size = new System.Drawing.Size(47, 13);
+            this.lblStreet1.TabIndex = 8;
+            this.lblStreet1.Text = "Street 1:";
+            // 
+            // lblStreet2
+            // 
+            this.lblStreet2.AutoSize = true;
+            this.lblStreet2.Location = new System.Drawing.Point(376, 80);
+            this.lblStreet2.Name = "lblStreet2";
+            this.lblStreet2.Size = new System.Drawing.Size(47, 13);
+            this.lblStreet2.TabIndex = 9;
+            this.lblStreet2.Text = "Street 2:";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Location = new System.Drawing.Point(12, 116);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(27, 13);
+            this.lblCity.TabIndex = 10;
+            this.lblCity.Text = "City:";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(261, 116);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(35, 13);
+            this.lblState.TabIndex = 11;
+            this.lblState.Text = "State:";
+            // 
+            // lblZipCode
+            // 
+            this.lblZipCode.AutoSize = true;
+            this.lblZipCode.Location = new System.Drawing.Point(478, 116);
+            this.lblZipCode.Name = "lblZipCode";
+            this.lblZipCode.Size = new System.Drawing.Size(53, 13);
+            this.lblZipCode.TabIndex = 12;
+            this.lblZipCode.Text = "Zip Code:";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(12, 157);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(41, 13);
+            this.lblPhone.TabIndex = 13;
+            this.lblPhone.Text = "Phone:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(78, 113);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(149, 20);
+            this.txtCity.TabIndex = 14;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(307, 113);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(149, 20);
+            this.txtState.TabIndex = 15;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(537, 113);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(128, 20);
+            this.txtZipCode.TabIndex = 16;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(78, 154);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(149, 20);
+            this.txtPhone.TabIndex = 17;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(266, 157);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(35, 13);
+            this.lblEmail.TabIndex = 18;
+            this.lblEmail.Text = "Email:";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(307, 154);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(207, 20);
+            this.txtEmail.TabIndex = 19;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(537, 154);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(128, 20);
+            this.btnSubmit.TabIndex = 20;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblFeedback
+            // 
+            this.lblFeedback.AutoSize = true;
+            this.lblFeedback.Location = new System.Drawing.Point(12, 218);
+            this.lblFeedback.Name = "lblFeedback";
+            this.lblFeedback.Size = new System.Drawing.Size(58, 13);
+            this.lblFeedback.TabIndex = 21;
+            this.lblFeedback.Text = "Feedback:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 459);
+            this.Controls.Add(this.lblFeedback);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblZipCode);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblStreet2);
+            this.Controls.Add(this.lblStreet1);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.lblMName);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.txtStreet2);
+            this.Controls.Add(this.txtStreet1);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtMName);
+            this.Controls.Add(this.txtFName);
+            this.Name = "Form1";
+            this.Text = "Lab 1 - Structures and Classes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.TextBox txtMName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtStreet1;
+        private System.Windows.Forms.TextBox txtStreet2;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.Label lblMName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label lblStreet1;
+        private System.Windows.Forms.Label lblStreet2;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblZipCode;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblFeedback;
+    }
+}
+
---
+++ Assign2_Sets&Gets/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Create instance of Person class called Nick
+            Person Nick = new Person();
+            Nick.FName = txtFName.Text;
+            Nick.MName = txtMName.Text;
+            Nick.LName = txtLName.Text;
+            Nick.Street1 = txtStreet1.Text;
+            Nick.Street2 = txtStreet2.Text;
+            Nick.City = txtCity.Text;
+            Nick.State = txtState.Text;
+            Nick.Zipcode = txtZipCode.Text;
+            Nick.Phone = txtPhone.Text;
+            Nick.Email = txtEmail.Text;
+
+            FillLabel(Nick);
+        }
+
+        public void FillLabel(Person temp)
+        {
+            lblFeedback.Text = temp.FName + "\n";
+            lblFeedback.Text += temp.MName + "\n";
+            lblFeedback.Text += temp.LName + "\n";
+            lblFeedback.Text += temp.Street1 + "\n";
+            lblFeedback.Text += temp.Street2 + "\n";
+            lblFeedback.Text += temp.City + "\n";
+            lblFeedback.Text += temp.State + "\n";
+            lblFeedback.Text += temp.Zipcode + "\n";
+            lblFeedback.Text += temp.Phone + "\n";
+            lblFeedback.Text += temp.Email + "\n";
+        }
+    }
+}
