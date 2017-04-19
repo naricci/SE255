258c3140b0daaf6f1b6a78f6ddb7d957d31ee740
Ricci <naricci@labs.neit.edu>
2017-04-19 08:55:48 -0400

    add remaining assignment instructions

---
+++ Assign3_DataValidation/WindowsFormsApplication1/Person.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    // Create class to represent Person object
+    public class Person
+    {
+        // Set private variables within Person class
+        private string fName;
+        private string mName;
+        private string lName;
+        private string street1;
+        private string street2;
+        private string city;
+        private string state;
+        private string zipcode;
+        private string phone;
+        private string email;
+
+        // Create public variables to use as the front-end for their private counterparts above
+        public string FName
+        {
+            // Accessor
+            get { return fName; }       // Returns a copy of the value stored in the private variable to the outside world
+            // Mutator
+            set                         // Allows the private variable to get the value of the public variable if it
+            {
+                fName = value;
+            }
+        }
+
+        public string MName
+        {
+            get { return mName; }
+            set
+            {
+                mName = value;
+            }
+        }
+
+        public string LName
+        {
+            get { return lName; }
+            set
+            {
+                lName = value;
+            }
+        }
+
+        public string Street1
+        {
+            get { return street1; }
+            set
+            {
+                street1 = value;
+            }
+        }
+
+        public string Street2
+        {
+            get { return street2; }
+            set
+            {
+                street2 = value;
+            }
+        }
+
+        public string City
+        {
+            get { return city; }
+            set
+            {
+                city = value;
+            }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+            }
+        }
+
+        public string Zipcode
+        {
+            get { return zipcode; }
+            set
+            {
+                zipcode = value;
+            }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set
+            {
+                phone = value;
+            }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+};
---
+++ Assign3_DataValidation/WindowsFormsApplication1/ValidationLibrary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Structure_Sample
+{
+    class ValidationLibrary
+    {
+
+        public static bool GotPoop(string temp)
+        {
+            bool result = false;
+
+            if (temp.Contains("Poop"))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+
+
+
+        //**********************************************************************************************
+        // Library of validation functions we can use in future projects
+        //**********************************************************************************************
+
+        //Receives a string and we can let user know if it is filled in
+        public static bool IsItFilledIn(string temp)
+        {
+            bool result = false;
+
+            if (temp.Length > 0)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+
+
+        //Receives a string and we can let user know if it is filled in
+        public static bool IsItFilledIn(string temp, int minlen)
+        {
+            bool result = false;
+
+            if (temp.Length >= minlen)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+
+
+
+
+        //Receives a string and we can let user know if it has a semi-valid email format
+        public static bool IsValidEmail(string temp)
+        {
+            //assume true, but look for bad stuff to make it false
+            bool result = true;
+
+            //Look for position of "@"
+            int atLocation = temp.IndexOf("@");
+            int NextatLocation = temp.IndexOf("@", atLocation+1);
+            
+            //temp = scott@neit.ca
+            // length = 13
+            // position of last period = 10
+            
+            //Look for position of last period "."
+            int periodLocation = temp.LastIndexOf(".");
+
+            //check for minimum length
+            if (temp.Length < 8)
+            {
+                result = false;
+            }
+            else if (atLocation < 2)    //if it is -1, not found and needs at least 2 chars in front
+            {
+                result = false;
+            }
+            else if (periodLocation + 2 > (temp.Length))
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+
+
+
+    }
+}
